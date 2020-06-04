a9790dbbabceddbd6e148ad338428a0a2e7774aa
jj <2380228977@qq.com>
2020-06-04 11:00:59 +0800

    框架搭建完成，可以运行

--- src/main/java/jj/controller/TestController.java
+++ src/main/java/jj/controller/TestController.java
@@ -1,22 +1,23 @@
 package jj.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
+@Slf4j
 @Controller
 @RequestMapping("/test")
 public class TestController {
 
-    private final static Logger logger = LoggerFactory.getLogger(TestController.class);
+    //private final static Logger logger = LoggerFactory.getLogger(TestController.class);
 
     @RequestMapping("/hello")
     @ResponseBody
     public String hello(){
-        logger.info("hello");
+        log.info("test permission");
         return "hello,permission";
     }
 }
